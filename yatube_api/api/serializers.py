32caeb34606fd0cbea36df7f1753c5c84f9d9a8c
Sergey Sokolkin <hawkxdev@gmail.com>
2025-08-12 19:23:25 +0300

    feat: add api serializers for all models

---
+++ yatube_api/api/serializers.py
@@ -0,0 +1,28 @@
+"""Сериализаторы для API приложения Yatube."""
+
+from rest_framework import serializers
+
+from posts.models import Group, Post, Comment
+
+
+class GroupSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Group
+        fields = ('id', 'title', 'slug', 'description')
+
+
+class PostSerializer(serializers.ModelSerializer):
+    author = serializers.StringRelatedField(read_only=True)
+
+    class Meta:
+        model = Post
+        fields = ('id', 'text', 'pub_date', 'author', 'image', 'group')
+
+
+class CommentSerializer(serializers.ModelSerializer):
+    author = serializers.StringRelatedField(read_only=True)
+
+    class Meta:
+        model = Comment
+        fields = ('id', 'author', 'post', 'text', 'created')
+        read_only_fields = ('post',)
