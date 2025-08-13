be14b5fa60167ae93a4007b0650ae43e14876092
Sergey Sokolkin <hawkxdev@gmail.com>
2025-08-13 09:48:50 +0300

    feat: enhance API views and serializers

--- yatube_api/api/serializers.py
+++ yatube_api/api/serializers.py
@@ -2,27 +2,33 @@
 
 from rest_framework import serializers
 
-from posts.models import Group, Post, Comment
+from posts.models import Comment, Group, Post
 
 
 class GroupSerializer(serializers.ModelSerializer):
+    """Сериализатор групп."""
+
     class Meta:
         model = Group
-        fields = ('id', 'title', 'slug', 'description')
+        fields = ("id", "title", "slug", "description")
 
 
 class PostSerializer(serializers.ModelSerializer):
+    """Сериализатор постов."""
+
     author = serializers.StringRelatedField(read_only=True)
 
     class Meta:
         model = Post
-        fields = ('id', 'text', 'pub_date', 'author', 'image', 'group')
+        fields = ("id", "text", "pub_date", "author", "image", "group")
 
 
 class CommentSerializer(serializers.ModelSerializer):
+    """Сериализатор комментариев."""
+    
     author = serializers.StringRelatedField(read_only=True)
 
     class Meta:
         model = Comment
-        fields = ('id', 'author', 'post', 'text', 'created')
-        read_only_fields = ('post',)
+        fields = ("id", "author", "post", "text", "created")
+        read_only_fields = ("post",)
--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -1,3 +1,69 @@
-from django.shortcuts import render
+"""ViewSets для API приложения Yatube."""
 
-# Create your views here.
+from typing import Any
+from django.db.models import QuerySet
+from rest_framework import viewsets
+from rest_framework.exceptions import PermissionDenied
+from rest_framework.serializers import BaseSerializer
+
+from posts.models import Comment, Group, Post
+from .serializers import CommentSerializer, GroupSerializer, PostSerializer
+
+
+class GroupViewSet(viewsets.ReadOnlyModelViewSet):
+    """Просмотр групп."""
+
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
+
+
+class PostViewSet(viewsets.ModelViewSet):
+    """CRUD постов."""
+
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+    def perform_create(self, serializer: BaseSerializer) -> None:
+        """Установка автора."""
+        serializer.save(author=self.request.user)
+
+    def perform_update(self, serializer: BaseSerializer) -> None:
+        """Проверка прав."""
+        if serializer.instance.author != self.request.user:
+            raise PermissionDenied("Изменение чужого контента запрещено!")
+        super().perform_update(serializer)
+
+    def perform_destroy(self, instance: Any) -> None:
+        """Проверка прав."""
+        if instance.author != self.request.user:
+            raise PermissionDenied("Удаление чужого контента запрещено!")
+        super().perform_destroy(instance)
+
+
+class CommentViewSet(viewsets.ModelViewSet):
+    """CRUD комментариев."""
+
+    serializer_class = CommentSerializer
+
+    def get_queryset(self) -> QuerySet[Comment]:
+        """Фильтрация по посту."""
+        post_id = self.kwargs.get("post_id")
+        return Comment.objects.filter(post=post_id)
+
+    def perform_create(self, serializer: BaseSerializer) -> None:
+        """Установка поста."""
+        post_id = self.kwargs.get("post_id")
+        post = Post.objects.get(id=post_id)
+        serializer.save(post=post, author=self.request.user)
+
+    def perform_update(self, serializer: BaseSerializer) -> None:
+        """Проверка прав."""
+        if serializer.instance.author != self.request.user:
+            raise PermissionDenied("Изменение чужого контента запрещено!")
+        super().perform_update(serializer)
+
+    def perform_destroy(self, instance: Any) -> None:
+        """Проверка прав."""
+        if instance.author != self.request.user:
+            raise PermissionDenied("Удаление чужого контента запрещено!")
+        super().perform_destroy(instance)
