ab5a98a7821513856f8a1a8141db4635a5d6f837
Sergey Sokolkin <hawkxdev@gmail.com>
2025-08-20 19:46:09 +0300

    refactor: update API routing and remove empty files

--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -6,17 +6,16 @@ from rest_framework.routers import DefaultRouter
 
 from .views import CommentViewSet, GroupViewSet, PostViewSet
 
-router = DefaultRouter()
-router.register('posts', PostViewSet)
-router.register('groups', GroupViewSet)
-router.register(
+router_v1 = DefaultRouter()
+router_v1.register('posts', PostViewSet)
+router_v1.register('groups', GroupViewSet)
+router_v1.register(
     r'posts/(?P<post_id>\d+)/comments',
     CommentViewSet,
     basename='post-comments'
 )
 
-
 urlpatterns = [
     path('v1/api-token-auth/', views.obtain_auth_token),
-    path('v1/', include(router.urls)),
+    path('v1/', include(router_v1.urls)),
 ]
